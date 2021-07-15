42001f4d5e424b7a9fbb79aaabef10770ca6b163
InsertAReallyCreativeNameHere <insertareallycreativenamehere@gmail.com>
2021-07-15 20:05:30 +1000

    Update Renderer.cpp

--- Marble.Runtime.GL/src/Rendering/Core/Renderer.cpp
+++ Marble.Runtime.GL/src/Rendering/Core/Renderer.cpp
@@ -336,7 +336,7 @@ void main()
 
     sampler2DTexturedPolygon = bgfx::createUniform("texColor", bgfx::UniformType::Sampler);
 
-    unitSquarePoly.create({ { -0.5f, -0.5f }, { 0.5f, -0.5f }, { 0.5f, 0.5f }, { -0.5f, 0.5f } }, { 0, 3, 2, 0, 2, 1 });
+    unitSquarePoly.create({ { -0.5f, -0.5f }, { 0.5f, -0.5f }, { 0.5f, 0.5f }, { -0.5f, 0.5f } }, { 2, 3, 0, 1, 2, 0 });
     unitTexturedSquarePoly.create({ { -0.5f, -0.5f, 0.0f, 0.0f }, { 0.5f, -0.5f, 1.0f, 0.0f }, { 0.5f, 0.5f, 1.0f, 1.0f }, { -0.5f, 0.5f, 0.0f, 1.0f } }, { 0, 3, 2, 0, 2, 1 });
     #pragma endregion
 
