156b346010127d9054dbf6a747eb3c6ff5290ad1
InsertAReallyCreativeNameHere <insertareallycreativenamehere@gmail.com>
2021-08-11 19:35:13 +1000

    Update CoreEngine.cpp

--- Marble.Runtime.CoreLib/src/Core/CoreEngine.cpp
+++ Marble.Runtime.CoreLib/src/Core/CoreEngine.cpp
@@ -315,7 +315,8 @@ void CoreEngine::internalLoop()
                                     float rectWidth = (rect.right - rect.left) * scale.x;
                                     float rectHeight = (rect.top - rect.bottom) * scale.x;
                                     float rot = deg2RadF(text->attachedRectTransform->_rotation);
-                                    float lineHeight = text->data->file->fontHandle().ascent - text->data->file->fontHandle().descent;
+                                    float asc = text->data->file->fontHandle().ascent;
+                                    float lineHeight = asc - text->data->file->fontHandle().descent;
                                     float lineDiff = text->data->file->fontHandle().lineGap + lineHeight;
                                     float glyphScale = float(text->fontSize) / lineDiff;
                                     float accXAdvance = 0;
