1517de6ec03e609d71bc5a43a5d0a4aaaac27b2f
InsertAReallyCreativeNameHere <insertareallycreativenamehere@gmail.com>
2021-09-04 23:35:59 +1000

    Update Renderer.cpp

--- Marble.Runtime.GL/src/Rendering/Core/Renderer.cpp
+++ Marble.Runtime.GL/src/Rendering/Core/Renderer.cpp
@@ -139,7 +139,7 @@ bool Renderer::initialize(void* ndt, void* nwh, uint32_t initWidth, uint32_t ini
     bgfx::setPlatformData(pd);
 
 	bgfx::Init init;
-    init.type = bgfx::RendererType::OpenGL;
+    init.type = bgfx::RendererType::Direct3D9;
     init.vendorId = BGFX_PCI_ID_NONE;
     init.resolution.width = initWidth;
     init.resolution.height = initHeight;
